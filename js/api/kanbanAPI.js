19d91227864dadf5bc3b321c06ad6c993f9e4f96
Tyler Spear <tylerspear@Tylers-iMac.local>
2023-03-06 22:00:38 -0600

    update column functionality

--- js/api/kanbanAPI.js
+++ js/api/kanbanAPI.js
@@ -26,11 +26,41 @@ export default class KanbanAPI {
         return item //ensure that the front end matches the local storage
     }
 
+    static updateItem(itemId, newProps) {
+        const data = read()
+        const [item,currentCol] = (() => {
+            for(const column of data) {
+                const item = column.items.find(item => item.id == itemId)
+                if(item){
+                    return [item,column]
+                }
+            }
+        })()
+
+        if(!item) {
+            throw new Error("Item not found")
+        }
+
+        item.content = newProps.content === undefined ? item.content : newProps.content   //if content has changes, update it, else remain unchanged
+
+        if(newProps.columnId !== undefined && newProps.position !== undefined) {
+            const targetCol = data.find(column => columnId == newProps.columnId) //changing column to be the new column once card is moved
+            if(!targetCol) {
+                throw new Error("Target column not found")
+            }
+
+            currentCol.items.splice(currentCol.items.indexOf(item), 1) //remove from old column
+
+            targetCol.items.splice(newProps.position, 0, item) //add to new column
+        }
+
+        save(data)
+    }
+
     static deleteItem(itemId) {
         const data = read()
         for(const column of data) {
             const item = column.items.find(item => item.id == itemId)
-            
             if(item) {
                 column.items.splice(column.items.indexOf(item), 1)
             }
